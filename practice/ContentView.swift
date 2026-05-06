4aa5b70b81500bb6abd4a4433f54e0b57fb64065
hiromuota166 <otahiromu166@gmail.com>
2026-05-06 21:40:51 +0900

    Initial Commit

---
+++ practice/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  practice
+//
+//  Created by 太田啓夢 on 2026/05/06.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+
+    var body: some View {
+        RealityView { content in
+
+            // Create a cube model
+            let model = Entity()
+            let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+            let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+            model.components.set(ModelComponent(mesh: mesh, materials: [material]))
+            model.position = [0, 0.05, 0]
+
+            // Create horizontal plane anchor for the content
+            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+            anchor.addChild(model)
+
+            // Add the horizontal plane anchor to the scene
+            content.add(anchor)
+
+            content.camera = .spatialTracking
+
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
